00749a84453b7dafdb21d2c2bf5c25ef354c8c8f
gmumdal <shinhs6384@naver.com>
2024-12-26 16:30:29 +0900

    너기

--- srcs/project/main/urls.py
+++ srcs/project/main/urls.py
@@ -1,9 +1,13 @@
+from django.contrib import admin
 from django.urls import path
-from .views import *
-
-app_name='main'
+from main.views import index, blog, posting
 
 urlpatterns = [
-	path('', index),
-	path('blog/', blog),
+    path('admin/', admin.site.urls),
+    # 웹사이트의 첫화면은 index 페이지이다 + URL이름은 index이다
+    path('', index, name='index'),
+    # URL:80/blog에 접속하면 blog 페이지 + URL이름은 blog이다
+    path('blog/', blog, name='blog'),
+    # URL:80/blog/숫자로 접속하면 게시글-세부페이지(posting)
+    path('blog/<int:pk>/',posting, name="posting"),
 ]
--- srcs/project/main/views.py
+++ srcs/project/main/views.py
@@ -7,3 +7,7 @@ def index(request):
 def blog(request):
 	postlist = Post.objects.all()
 	return render(request, 'main/blog.html', {'postlist':postlist})
+
+def posting(request, pk):
+	post = Post.objects.get(pk=pk)
+	return render(request, 'main/posting.html', {'post':post})
