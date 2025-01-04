ccb0973f595bb5e40d37a1ec27fb6e08c798e491
gmumdal <shinhs6384@naver.com>
2025-01-04 12:15:23 +0900

    2-04 find, template

--- srcs/project/pybo/views.py
+++ srcs/project/pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 def index(request):
@@ -7,3 +7,9 @@ def index(request):
 	# render 함수는 파이썬 데이터를 템플릿에 적용하여 HTML로 반환하는 함수
 	# question_list 데이터를 html 파일에 적용하여 생성한 후 리턴해준다. 
 	return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+	# objects 중 get으로 얻는 방식 -> pk가 question_id인 값을 얻거나, 404를 받는 방식
+	question = get_object_or_404(Question, pk=question_id)
+	context = {'question': question}
+	return render(request, 'pybo/question_detail.html', context)
