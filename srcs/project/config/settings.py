3331fad443c626a7553c4c5bc1140e193435d3be
gmumdal <shinhs6384@naver.com>
2025-01-05 16:33:10 +0900

    3-05 login logout

--- srcs/project/config/settings.py
+++ srcs/project/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'common.apps.CommonConfig',
 	'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -125,3 +126,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
