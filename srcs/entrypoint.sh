78bebe32270954bf8629f92b05eaf8751ca44e9c
gmumdal <shinhs6384@naver.com>
2025-01-08 18:55:50 +0900

    oauth start

--- srcs/entrypoint.sh
+++ srcs/entrypoint.sh
@@ -7,10 +7,5 @@ apt-get install -y procps
 
 pip install --no-cache-dir -r /home/requirements.txt
 test -f manage.py || django-admin startproject mysite .
-mkdir -p /logs
-touch /logs/django.log
-chmod 755 /logs/django.log
 
-tail -f /logs/django.log
-
-#python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
